efd0454ed97d3f1b47b70d2a6ea74124e55aee6c
Matheus Ancelmo <Matheus Ancelmo@DESKTOP-RR7IH9N>
2017-09-23 15:59:29 -0300

    habemus um cliente

--- Server_Pi_v2/src/client/Cliente.java
+++ Server_Pi_v2/src/client/Cliente.java
@@ -6,13 +6,41 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import client.Encrypt;
+
 public class Cliente {
 
 	public static void main(String[] args) {
 		
 		String key = "1234567891234567";
 		
-
+		try{
+			while(true){
+				//Captura input do teclado
+				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+				System.out.println("Insira o texto a ser enviado: ");
+				String text = in.readLine();
+			
+				//Encripta a leitura
+				text = Encrypt.encrypt(text, key);
+				System.out.println("Cliente- Cypher: "+text);
+			
+				//Cria o socket com o nome do servidor e a porta
+				Socket client = new Socket("Ferretti-PC", 10010);	
+				System.out.println("Cliente- CONECTADO");
+			
+				//Envia para o servidor
+				ObjectOutputStream envio = new ObjectOutputStream(client.getOutputStream());
+				envio.writeObject(text);
+			
+				//Encerra o processo
+				envio.close();
+				System.out.println("Cliente- Conexão encerrada");
+			}
+		}
+		catch(Exception e){
+			System.out.println("Cliente: ERRO!" +e.getMessage());			
+		}
 	}
-
 }
+
---
+++ Server_Pi_v2/src/client/Encrypt.java
@@ -0,0 +1,22 @@
+package client;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class Encrypt {
+	
+	public static String encrypt(String input, String key){
+		  byte[] crypted = null;
+		  try{
+		    SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
+		      Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		      cipher.init(Cipher.ENCRYPT_MODE, skey);
+		      crypted = cipher.doFinal(input.getBytes());
+		    }catch(Exception e){
+		    	System.out.println(e.toString());
+		    }
+		    return new String(Base64.encodeBase64(crypted));
+		}
+}
