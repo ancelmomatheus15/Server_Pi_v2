a9d29a21412b314584ecc303d344dbc43584d02c
Matheus Ancelmo <Matheus Ancelmo@DESKTOP-RR7IH9N>
2017-09-23 14:59:45 -0300

    pega ai

---
+++ Server_Pi_v2/src/client/Cliente.java
@@ -0,0 +1,18 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		
+		String key = "1234567891234567";
+		
+
+	}
+
+}
