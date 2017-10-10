0b6a528f1651169e3b312119e1f8be8b0190ca3d
Matheus Ancelmo <Matheus Ancelmo@DESKTOP-RR7IH9N>
2017-10-10 17:48:24 -0300

    TA PRONTO FALTA DOCUMENTAR AAAAAAAAAAAAA

--- Server_Pi_v2/src/client/Cliente.java
+++ Server_Pi_v2/src/client/Cliente.java
@@ -14,55 +14,55 @@ public class Cliente {
 	public static void main(String[] args) {
 		
 		String key = "1234567891234567";
-		
-		try{
-			//Cria o socket com o nome do servidor e a porta
-			Socket client = new Socket("Ferretti-PC", 10010);	
-			System.out.println("Cliente- CONECTADO");
-			
+					
 			while(true){
-				
-				//Pega IP e MAC
-				String mac = HandshakeClient.HandShake();
-								
-				//Captura input do teclado
-				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("Insira o texto a ser enviado: ");
-				String text = in.readLine();
-				
-				//Encripta a leitura
-				text = Encrypt.encrypt(text, key);
-				System.out.println("Cliente- Cypher: "+text);
+				try{
+					//Cria o socket com o nome do servidor e a porta
+					Socket client = new Socket("Ferretti-PC", 10010);	
+					System.out.println("Cliente- CONECTADO");
 			
-				//Calculo do CRC
-				String CRCvalue = CRC.calcCRC(text);
-				System.out.println("CRC: "+CRCvalue);
+					//Pega IP e MAC
+					String mac = HandshakeClient.HandShake();
+								
+					//Captura input do teclado
+					BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+					System.out.println("Insira o texto a ser enviado: ");
+					String text = in.readLine();
 				
-				//Calculo do Checksum
-				String checksumValue = Checksum.md5(text);
-				System.out.println("Checksum: "+checksumValue);
+					//Encripta a leitura
+					text = Encrypt.encrypt(text, key);
+					System.out.println("Cliente- Cypher: "+text);
+			
+					//Calculo do CRC
+					String CRCvalue = CRC.calcCRC(text);
+					System.out.println("CRC: "+CRCvalue);
 				
-				//adicionando parametros de vericação ao String principal de texto
-				String aux = text;
-				text = mac.concat(CRCvalue); //MAC + CRC
-				text = text.concat(checksumValue); //MAC + CRC + CHECKSUM
-				text = text.concat(aux); //MAC + CRC + CHECKSUM + INFO
-				System.out.println(text+" "+text.length());
+					//Calculo do Checksum
+					String checksumValue = Checksum.md5(text);
+					System.out.println("Checksum: "+checksumValue);
+				
+					//adicionando parametros de vericação ao String principal de texto
+					String aux = text;
+					text = mac.concat(CRCvalue); //MAC + CRC
+					text = text.concat(checksumValue); //MAC + CRC + CHECKSUM
+					text = text.concat(aux); //MAC + CRC + CHECKSUM + INFO
+					System.out.println(text+" "+text.length());
 		
 				
-				//Envia para o servidor
-				ObjectOutputStream envio = new ObjectOutputStream(client.getOutputStream());
-				envio.writeObject(text);
-				envio.flush();
+					//Envia para o servidor
+					ObjectOutputStream envio = new ObjectOutputStream(client.getOutputStream());
+					envio.writeObject(text);
+					envio.flush();
 			
-				//Encerra o processo
-				envio.close();
-				System.out.println("Cliente- Conexão encerrada");
-			}
+					//Encerra o processo
+					envio.close();
+					System.out.println("Cliente- Conexão encerrada");
+				}
+				catch(Exception e){
+					System.out.println("Cliente: ERRO!" +e.getMessage());			
+				}
 		}
-		catch(Exception e){
-			System.out.println("Cliente: ERRO!" +e.getMessage());			
-		}
+		
 	}
 }
 
